a68fa1a0c7c5b62ab4b70124e91d519f90eb6e16
Dan Connolly <http://www.madmode.com/>
2016-11-23 15:00:09 -0600

    use tag_safe plugin with ocap tag

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-toward least-authority lint for rust: test lint works
+# larust: toward least-authority lint for rust
+
+src/main.rs uses the `tag_safe` lint with an `ocap` tag.
+
+`cargo run` should work as usual.
+
+## Earlier work
 
  1. to build `liblint_plugin_test.so`:
     - `rustc lint_plugin_test.rs`
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+#![feature(custom_attribute,plugin)]
+#![plugin(tag_safe)]
+
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+  let stdout = io::stdout();
+  let mut handle = stdout.lock();
+  match cap_main(&mut handle) {
+    Ok(_) => (),
+    err => write!(io::stderr(), "oops!: {:?}", err).unwrap()
+  }
+}
+
+#[tag_safe(ocap)]
+fn cap_main<W>(out: &mut W) -> io::Result<()>
+  where W: Write
+{
+  say_stuff(out)
+}
+
+fn say_stuff<W>(out: &mut W) -> io::Result<()>
+  where W: Write
+{
+  say_hi(out)
+}
+
+fn say_hi<W>(out: &mut W) -> io::Result<()>
+  where W: Write
+{
+  write!(out, "Hello, world!")
 }
